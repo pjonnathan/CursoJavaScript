27b1ee3378f64f61de59aca6cbdb31e9e5830c20
Pedro_Jonnathan <43188930+pjonnathan@users.noreply.github.com>
2023-02-28 09:24:06 -0300

    add funcion

---
+++ Curso_JavaScript/AULA016/funcao01.js
@@ -0,0 +1,9 @@
+function verificador(n){
+    if (n%2==0){
+        return 'par'
+    }else{
+        return 'impar'
+    }
+}
+let res = verificador(5)
+console.log(res)
---
+++ Curso_JavaScript/AULA016/funcao02.js
@@ -0,0 +1,9 @@
+let v = function(n1,n2){
+    return n1*n2
+}
+console.log(v(9,8))
+
+function soma(n1,n2){
+    return n1+n2
+}
+console.log(soma(5,9))
